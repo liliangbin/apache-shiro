04d33ec30a5f2159b42277d9a03c199781a8b5a4
liliangbin <2286824491@qq.com>
2018-01-20 11:41:04 +0800

    添加了缓存，记住我的操作。完结撒花

--- src/main/java/com/example/app/shiro/demo/config/ShiroConfiguration.java
+++ src/main/java/com/example/app/shiro/demo/config/ShiroConfiguration.java
@@ -1,13 +1,16 @@
 package com.example.app.shiro.demo.config;
 
 import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
+import org.apache.shiro.cache.ehcache.EhCacheManager;
 import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.mgt.SecurityManager;
 
 import org.apache.shiro.util.ByteSource;
+import org.apache.shiro.web.mgt.CookieRememberMeManager;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 
+import org.apache.shiro.web.servlet.SimpleCookie;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -48,6 +51,16 @@ public class ShiroConfiguration {
 
         //配置退出过滤器，其中的具体的退出的代码，shiro已经帮我们写好了。
         filterChainDefinitionMap.put("/logout", "logout");
+
+
+
+        //配置记住我或认证通过可以访问的地址
+
+        filterChainDefinitionMap.put("/index", "user");
+
+        filterChainDefinitionMap.put("/", "user");
+
+
 
         //<!-- 过滤链定义，从上向下顺序执行，一般将 /**放在最为下边 -->:这是一个坑呢，一不小心代码就不好使了;
 
@@ -146,6 +159,85 @@ public class ShiroConfiguration {
         return authorizationAttributeSourceAdvisor;
 
     }
+
+    /**
+
+     * shiro缓存管理器;
+
+     * 需要注入对应的其它的实体类中：
+
+     * 1、安全管理器：securityManager
+
+     * 可见securityManager是整个shiro的核心；
+
+     * @return
+
+     */
+
+    @Bean
+
+    public EhCacheManager ehCacheManager(){
+
+        System.out.println("ShiroConfiguration.getEhCacheManager()");
+
+        EhCacheManager cacheManager = new EhCacheManager();
+
+        cacheManager.setCacheManagerConfigFile("classpath:config/ehcache-shiro.xml");
+
+        return cacheManager;
+
+    }
+
+    /**
+
+     * cookie对象;
+
+     * @return
+
+     */
+
+    @Bean
+
+    public SimpleCookie rememberMeCookie(){
+
+        System.out.println("ShiroConfiguration.rememberMeCookie()");
+
+        //这个参数是cookie的名称，对应前端的checkbox的name = rememberMe
+
+        SimpleCookie simpleCookie = new SimpleCookie("rememberMe");
+
+        //<!-- 记住我cookie生效时间30天 ,单位秒;-->
+
+        simpleCookie.setMaxAge(259200);
+
+        return simpleCookie;
+
+    }
+
+
+
+    /**
+
+     * cookie管理对象;
+
+     * @return
+
+     */
+
+    @Bean
+
+    public CookieRememberMeManager rememberMeManager(){
+
+        System.out.println("ShiroConfiguration.rememberMeManager()");
+
+        CookieRememberMeManager cookieRememberMeManager = new CookieRememberMeManager();
+
+        cookieRememberMeManager.setCookie(rememberMeCookie());
+
+        return cookieRememberMeManager;
+
+    }
+
     @Bean
 
     public SecurityManager securityManager(){
@@ -155,7 +247,13 @@ public class ShiroConfiguration {
         //设置realm.
 
         securityManager.setRealm(myShiroRealm());
+        //注入缓存管理器;
 
+        securityManager.setCacheManager(ehCacheManager());//这个如果执行多次，也是同样的一个对象;
+
+        //注入记住我管理器;
+
+        securityManager.setRememberMeManager(rememberMeManager());
         return securityManager;
 
     }
